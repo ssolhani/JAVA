35f7a022da5940dce2fc203eb65eb60d0042581a
ssolhani <78973232+ssolhani@users.noreply.github.com>
2021-03-30 17:13:12 +0900

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,21 @@
+public class Datatype{
+	public static void main(String[] args) {
+		System.out.println(6); //Number
+		System.out.println("six"); //String
+		
+		System.out.println("6"); //String 6
+		
+		System.out.println(6+6); //12
+		System.out.println("6"+"6");
+		System.out.println(6*6); //36
+		//System.out.println("6"*"6");
+		
+		System.out.println("1111".length());
+		//System.out.println(1111.length());
+		
+		
+        System.out.println("Hello World"); //String 문자열
+        System.out.println('H'); //Char 문자
+        System.out.println("H");
+	} 
+}
